fd8f91e4175c596c3d4922ac5bd6738bcd0b04ef
kasi-iyo <snakeman143@gmail.com>
2020-06-04 10:20:46 +0900

    Revert "マイグレーションミス"
    This reverts commit 3ba5beaa5bdbf1431631285569941782efe793e6.

--- app/models/novel_tag_map.rb
+++
@@ -1,4 +0,0 @@
-class NovelTagMap < ApplicationRecord
-  belongs_to :novel
-  belongs_to :novel_tag
-end
--- db/migrate/20200603114505_create_novel_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateNovelTags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :novel_tags do |t|
-      t.string :novel_tag_name
-
-      t.timestamps
-    end
-  end
-end
