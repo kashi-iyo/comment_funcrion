028d205a7865e1dad8fda949119f67b86e746f9e
kasi-iyo <snakeman143@gmail.com>
2020-06-04 18:18:28 +0900

    Postモデルに、パラメタで受け取ったタグを保存する機能を抽出

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,19 @@ class Post < ApplicationRecord
 
   has_many :post_tag_maps, dependent: :destroy
   has_many :post_tags, through: :post_tag_maps
+
+  def save_tag(sent_tags)
+    current_tags = self.post_tags.pluck(:post_tag_name) unless self.post_tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.post_tags.delete PostTag.find_by(post_tag_name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = PostTag.find_or_create_by(post_tag_name: new)
+      self.post_tags << new_post_tag
+    end
+  end
 end
